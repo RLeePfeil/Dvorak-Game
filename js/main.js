b32dc0f81bea519e43ea487ca4a609fe1cbce896
Lee Pfeil <lee@gripmedia.com>
2014-04-13 13:04:36 -0700

    add stats

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+var stats = {
+	startTime: null,
+	numChars: 0,
+	numCorrectChars: 0,
+	numIncorrectChars: 0,
+	numWords: 0,
+	incorrectChars: {}
+}
+
 $(function(){
 	addSentence('the quick brown fox jumps over the lazy dog');
 	//$('body').on('keypress', handleKeypress);
@@ -23,9 +32,14 @@ function addSentence(sentence) {
 function onDownCallback(e, a, c) {
 	e.preventDefault();
 	
-	console.log(e);
-	console.log(a);
-	console.log(c);
+	// Start stats timer
+	if (stats.startTime == null) {
+		stats.startTime = Date.now();
+	}
+	
+	//console.log(e);
+	//console.log(a);
+	//console.log(c);
 	
 	var $current = $('span.current', '#sentence');
 	if (c == 'backspace') {
@@ -33,6 +47,10 @@ function onDownCallback(e, a, c) {
 		if ($prev.length) {
 			$prev.removeClass('correct incorrect').addClass('current');
 			$current.removeClass('correct incorrect current');
+			stats.numChars--;
+			if ($prev.html() == ' ') {
+				stats.numWords--;
+			}
 		}
 		
 		return false;
@@ -44,15 +62,25 @@ function onDownCallback(e, a, c) {
 	
 	if (compareKeys($current.html(), a, c)) {
 		$current.addClass('correct');
+		stats.numCorrectChars++;
+		
+		if (a[a.length-1] == 'spacebar') {
+			stats.numWords++;
+		}
 	} else {
 		$current.addClass('incorrect');
+		stats.numIncorrectChars++;
 	}
+	
+	stats.numChars ++;
 	
 	$next = $current.next();
 	if ($next.length) {
 		$next.addClass('current');
 		$current.removeClass('current');
 	}
+	
+	updateStats();
 	
 	return false;
 }
@@ -69,4 +97,15 @@ function compareKeys(html, array, c) {
 	}
 	
 	return false;
+}
+
+function updateStats() {
+	var min = ((Date.now() - stats.startTime) / 60000);
+	var accuracy = Math.round((stats.numChars - stats.numIncorrectChars) / stats.numChars * 100, 2);
+	var wpm = Math.round(stats.numWords / min);
+	var cpm = Math.round(stats.numChars / min);
+	
+	$('#stat-accuracy').html(accuracy+'%');
+	$('#stat-wpm').html(wpm);
+	$('#stat-cpm').html(cpm);
 }
